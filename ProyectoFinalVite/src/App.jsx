16f27296d81b9281c1d55e772019d4d096b92385
Guadalupe Fernandez E <gfetchemendi@gmail.com>
2023-11-13 19:19:59 -0300

    actualizacion desde developer

--- ProyectoFinalVite/src/App.jsx
+++ ProyectoFinalVite/src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import ListaClientes from './componentes/ListaClientes';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from "react-router-dom";
-
 import { NavBar } from './componentes/NavBar';
 import Footer from './componentes/Footer';
 import { Detail } from './componentes/Detail';
 import CrearClienteForm from './componentes/crearClienteForm';
-
 import ModificarClienteForm from './componentes/modificarClienteForm';
-
 
 function App() {
   
@@ -20,9 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ListaClientes />} />
           <Route path={"/details/id/:id"} element={<Detail />} />
-
           <Route path={"/modificar/:id"} element={<ModificarClienteForm />} />
->>>>>>> 50c6074a262ae22688433bd8ecd9ee02b4b0be1c
           <Route path="/registrar" element={<CrearClienteForm />} />
        </Routes>  
     <Footer/>
