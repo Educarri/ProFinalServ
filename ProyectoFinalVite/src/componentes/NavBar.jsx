db25c7f7800afa659007cc532b5c711e59b59b93
karinacrognale <109962932+karinacrognale@users.noreply.github.com>
2023-11-15 19:25:45 -0300

    ¨actualizacion¨

--- ProyectoFinalVite/src/componentes/NavBar.jsx
+++ ProyectoFinalVite/src/componentes/NavBar.jsx
@@ -19,6 +19,7 @@ export const NavBar = () => {
     <nav className="navbar navbar-dark bg-dark" aria-label="First navbar example" >
     <div className="container-fluid">
       <Link className="link" to={"/"}>Servicios</Link>
+      <img src='/src/assets/logo.png' alt="logo" />
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
