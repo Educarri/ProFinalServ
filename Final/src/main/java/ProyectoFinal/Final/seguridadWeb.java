96160a4268793d23b8e4a715d6578e49ba29dceb
Guadalupe Fernandez E <gfetchemendi@gmail.com>
2023-12-01 20:24:19 -0300

    actualizacion estilos 1-12

--- Final/src/main/java/ProyectoFinal/Final/seguridadWeb.java
+++ Final/src/main/java/ProyectoFinal/Final/seguridadWeb.java
@@ -1,6 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ProyectoFinal.Final;
 
-import ProyectoFinal.Final.servicios.UsuarioService;
 import ProyectoFinal.Final.servicios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
